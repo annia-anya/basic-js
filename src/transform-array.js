dc886e2506c8e67404abd4644bee216b61c1e940
Hanna Shatsko <NytaShetko@yandex.ru>
2021-03-01 23:55:34 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if (!(Array.isArray(arr))) {
+        throw new Error();
+    }
+
+    let discardNext = false;
+    let newArr = [];
+    const length = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--double-next') {
+          discardNext = false;
+            if (i + 1 < length) {
+                newArr.push(arr[i + 1]);
+            }
+        } else if (arr[i] === '--double-prev') {
+            if ((i - 1 > 0) && !discardNext) {
+                newArr.push(arr[i - 1]);
+            }
+            discardNext = false;
+        } else if (arr[i] === '--discard-next') {
+            discardNext = true;
+            i++;
+        } else if (arr[i] === '--discard-prev') {
+            if(!discardNext) {
+              newArr.pop();
+            }
+            discardNext = false;
+        } else {
+            newArr.push(arr[i]);
+            discardNext = false;
+        }
+    }
+    return newArr;
+}
