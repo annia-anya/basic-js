456d1c4302c183cab684facb4fab7ebd7ebce243
Hanna Shatsko <NytaShetko@yandex.ru>
2021-03-04 00:15:54 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direct = true) {
+    this.direct = direct;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+    let result = '';
+    let currentOffsetIndex = 0;
+    const normalizedKey = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      const normalizedChar = message[i].toUpperCase();
+      if (normalizedChar >= 'A' && normalizedChar <= 'Z') {
+        const offset = normalizedKey[currentOffsetIndex % normalizedKey.length].charCodeAt(0) - 'A'.charCodeAt(0);
+        result += String.fromCharCode((normalizedChar.charCodeAt(0) - 65 + offset) % 26 + 65);
+        currentOffsetIndex++;
+      } else {
+        result += normalizedChar;
+      }
+    }
+    if (!this.direct) {
+      result = result.split('').reverse().join('');
+    }
+    return result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+    let result = '';
+    let currentOffsetIndex = 0;
+    const normalizedKey = key.toUpperCase();
+    for (let i = 0; i < message.length; i++) {
+      const normalizedChar = message[i].toUpperCase();
+      if (normalizedChar >= 'A' && normalizedChar <= 'Z') {
+        const offset = normalizedKey[currentOffsetIndex % normalizedKey.length].charCodeAt(0) - 'A'.charCodeAt(0);
+        result += String.fromCharCode(((normalizedChar.charCodeAt(0) - offset - 65) % 26 + 26) % 26 + 65);
+        currentOffsetIndex++;
+      } else {
+        result += normalizedChar;
+      }
+    }
+    if (!this.direct) {
+      result = result.split('').reverse().join('');
+    }
+    return result.toUpperCase();
   }
 }
 
